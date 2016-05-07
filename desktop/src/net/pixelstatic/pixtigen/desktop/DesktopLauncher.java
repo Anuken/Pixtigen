ed86095996cd7c47d0a8aab4ed4e7b2bb0756c57
BuildTools <arnukren@gmail.com>
2016-05-06 21:48:14 -0400

    Initial commit.

---
+++ desktop/src/net/pixelstatic/pixtigen/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.pixelstatic.pixtigen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.pixelstatic.pixtigen.Pixtigen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pixtigen(), config);
+	}
+}
---
+++ html/src/net/pixelstatic/pixtigen/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.pixelstatic.pixtigen.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.pixelstatic.pixtigen.Pixtigen;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Pixtigen();
+        }
+}
